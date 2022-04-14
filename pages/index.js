4df6dec239076f5d88d62ff7c9d68c8d7601d7db
Jonathan Libov <jonathan@whoo.ps>
2022-04-13 21:25:29 -0400

    forgot to add ssr

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import { createCanvas } from 'canvas';
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Hello cruel world</h1>
+    <h1>{props.message}</h1>
   )
+}
+
+export async function getServerSideProps() {
+  
+  return { props: { message: "Hello cruel world" } }
 }
